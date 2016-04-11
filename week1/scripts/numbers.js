567b5d2f924864f3abc10af117f84f74f4d3cafd
patrickmccall <patrick.mccall@gmail.com>
2016-04-11 10:08:58 -0700

    first commit Homework 1

---
+++ week1/scripts/numbers.js
@@ -0,0 +1,50 @@
+﻿var valueCount = 0;
+var valueSum = 0;
+var valueAverage = "";
+
+$(document).ready(ResetValues)
+
+
+function UpdateValues() {
+  var inputValue;
+
+  inputValue = $("#inputValue").val();
+  console.log(inputValue);
+
+  //test that the input is a valid entry
+  if ($.isNumeric(inputValue)) {
+    //convert it to a number
+    inputValue = Number(inputValue);
+    //console.log("success");
+    //Keep track of the counts and update the page
+    valueCount++;
+    console.log("count: " + valueCount);
+    $("#count").html(valueCount);
+
+    //do the same for the running total (sum) of the values
+    valueSum += inputValue;
+    console.log("  sum: " + valueSum);
+    $("#sum").html(valueSum);
+
+    //finally compute the average using the two variables sum divided by count
+    valueAverage = valueSum / valueCount;
+    console.log("  avg: " + valueAverage);
+    $("#average").html(valueAverage);
+  }
+  //it's not valid, let the user know
+  else {
+    console.log("fail");
+    //alert("please enter a number")
+
+  }
+}
+
+function ResetValues() {
+  valueCount = 0;
+  valueSum = 0;
+  valueAverage = "";
+
+  $("#count").html(valueCount);
+  $("#sum").html(valueSum);
+  $("#average").html(valueAverage);
+}
