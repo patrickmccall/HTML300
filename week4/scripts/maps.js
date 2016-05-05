9a851277b22b980650b1f2d2dd1ff423bf54e46e
Patrick McCall <patrick.mccall@gmail.com>
2016-05-04 21:26:46 -0700

    hw4 goodnuff

--- week4/scripts/maps.js
+++ week4/scripts/maps.js
@@ -3,45 +3,21 @@ var locations = '{"locations":[' +
 '{"name":"Arc De Triumph","coordinates":"48.873954, 2.295038"},' +
 '{"name":"Stonehenge","coordinates":"51.179050, -1.826215" },' +
 '{"name":"Bosporus Bridge","coordinates":"41.045990, 29.034033" },' +
-'{"name":"General Sherman Tree","coordinates":"6.581853, -118.751442" },' +
+'{"name":"General Sherman Tree","coordinates":"36.581974, -118.751421" },' +
 '{"name":"Philippe The Original","coordinates":"34.059989, -118.237016" },' +
 '{"name":"Sasebo, Japan","coordinates":"33.191988, 129.711714" }' +
 ']}';
 
-//var text = {
-//    "locations": [{
-//        "name": "Arc D'Triumph"
-//        , "coordinates": "48.873954, 2.295038"
-//    }
-//    , {
-//        "name": "Stonehenge"
-//        , "coordinates": "51.179050, -1.826215"
-//    },
-//    {
-//        "name": "Bosporus Bridge"
-//        , "coordinates": "41.045990, 29.034033"
-//    },
-//    {
-//        "name": "General Sherman Tree"
-//        , "coordinates": "36.581853, -118.751442"
-//    },
-//    {
-//        "name": "Philippe The Original"
-//        , "coordinates": "34.059989, -118.237016"
-//    },
-//    {
-//        "name": "Sasebo, Japan"
-//        , "coordinates": "33.191988, 129.711714"
-//    }
-//    ]
-//};
 
 
+var leafletMap;
 
 $(document).ready(function () {
     setupPlaces();
 
     $("select").on("change", goToSelectedPlace);
+
+    buildMap("48.873954, 2.295038");
 })
 
 
@@ -58,7 +34,7 @@ function setupPlaces() {
         name = places["locations"][i].name;
         coordinates = places["locations"][i].coordinates;
 
-        $("#places").append('<option value="' + coordinates + '" >' + name + '</option>');
+        $("#places").append('<option value="' + coordinates + '" name="' + name + '">' + name + '</option>');
 
 
     }
@@ -71,21 +47,34 @@ function setupPlaces() {
 
 function goToSelectedPlace(evt) {
     var coordinates = evt.currentTarget.value;
-    buildMap(coordinates);
+
+    //var leafletMap = L.map('leaflet-map'); //The id of the div
+    var latLng = JSON.parse("[" + coordinates + "]");
+
+    leafletMap.setView(latLng, 16);
+
+    var marker = L.marker(latLng);
+    marker.bindPopup(JSON.stringify(latLng));
+    marker.addTo(leafletMap);
 }
 
 
 function buildMap(coordinates) {
-    var leafletMap = L.map('leaflet-map'); //The id of the div
+    leafletMap = L.map('leaflet-map'); //The id of the div
+    var latLng = JSON.parse("[" + coordinates + "]");
+
+    leafletMap.setView(latLng, 13);
+
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
         attribution: '© <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
         maxzoom: 19
     }).addTo(leafletMap);
 
-    leafletMap.setView([coordinates], 16);
-
-    var marker = L.marker([coordinates]);
-    marker.bindPopup(coordinates);
+    var marker = L.marker(latLng);
+    marker.bindPopup(JSON.stringify(latLng));
     marker.addTo(leafletMap);
+    
+    //marker.bindPopup("<b>" + evt.currentTarget.html + "</b><br>I am a popup.");
+    
 }
