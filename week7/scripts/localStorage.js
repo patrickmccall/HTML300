6049207e6ecc4616abd67c597e4be2ce72e735f4
patrickmccall <patrick.mccall@gmail.com>
2016-05-25 16:47:08 -0700

    homework7

---
+++ week7/scripts/localStorage.js
@@ -0,0 +1,80 @@
+﻿var settings = {
+  name: ""
+};
+
+$(document).ready(function () {
+
+  $("#submit").on("click", UpdateSettings);
+
+
+  //check if there is something in local storage and get it
+  // but only if we're on the hello page
+  var url = window.location.pathname ;
+  if (url == "/week7/hello-page.html" ) {
+    GetSettings();
+  }
+
+
+  //wire up the other buttons
+  $("#editSettings").on("click", GoTo);
+  $("#clearSettings").on("click", GoTo);
+})
+
+function UpdateSettings(evt) {
+  var name = $("#name").val();
+  //console.log(name);
+  if (name == "") {
+    //console.log("empty");
+
+  }
+  else {
+    //console.log("not empty")
+    settings["name"] = name;
+    localStorage.setItem("settingsKey",JSON.stringify(settings));
+    //console.log(JSON.stringify(settings));
+    var value = localStorage.getItem("settingsKey");
+    //console.log(JSON.parse(value));
+  }
+  location.href = "hello-page.html";
+}
+function GetSettings() {
+
+  var value = localStorage.getItem("settingsKey");
+  var settings = "";
+  if (value != null) {
+    settings = JSON.parse(value);
+    $("#name").html(settings["name"]);
+  }
+  else {
+    //there's nothing, so go to the settings input page
+    location.href = "form-page.html";
+  }
+
+}
+
+function ClearSettings() {
+  localStorage.clear();
+  }
+
+function GoTo(evt) {
+  //console.log(evt.target.id);
+  var caller = evt.target.id;
+
+  switch (caller) {
+    case "editSettings":
+      location.href = "form-page.html";
+    case "clearSettings":
+      ClearSettings();
+      GetSettings();
+    default: location.href = "#";
+
+  }
+  //if (evt.target.id == "editSettings") {
+  //  console.log(evt.target.id);
+  //  location.href = "http://www.google.com";
+  //}
+
+  //evt.preventDefault();
+  //e.stopPropagation();
+
+}
